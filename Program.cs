f188a5dfcf3bf42695c3819465529251857d6818
AlexKuro <89215682813@mail.ru>
2022-12-24 23:19:35 +0300

    Метод - Task1 Вводит тект компьютер

--- Program.cs
+++ Program.cs
@@ -5,6 +5,25 @@ class Program
     {
 
 
+        void Task1() // Вводит тект компьютер
+        {
+            Console.WriteLine("\tВводит тект компьютер.\n");
+            string[] container = { "Hello", "-2", "world", "МИР", "Hi", "GeekBrains", "Student", ";-)", ":-)", ":0", "99",
+                                   "1", "CVC", "NO", "YES", "True", "False", "Go", "Russia", "Kazan", "Search", "Ru", "$",
+                                   "En", "-6", "0", "Z", "One", "Mail", "Pro", "Program", "7", "77", ".cs", ".txt", "BMW",
+                                   "VW", "Audi", "bin", "odj", "Труд", "МАЙ", "Пн", "Вт", "Cp", "Чт", "Пт", "СБ", "BC", "3",
+                                   "One", "Two", "Three", "Four", "Five",   "Six",  "Seven", "Eight", "Nine", "Ten"};
+            Random rand = new Random();
+            int size = 6;
+            string[] text = new string[size];
+            for (int i = 0; i < size; i++) text[i] = container[rand.Next(0, container.Length)];
+            Console.Write("\t");
+            PrintArrayStr(text);
+            Console.Write(" --> ");
+            PrintArrayStr(SearchByLines(text));
+            Console.WriteLine();
+        }
+
         string[] SearchByLines(string[] text, int lenNum = 3) // поиск нужных элементов по длине строки 
         {
             int size = text.Length;
@@ -24,6 +43,8 @@ class Program
             for (int i = 0; i < pArray.Length; i++) Console.Write($"{pArray[i]} ");
             Console.Write("]");
         }
+
+
 
     }
 }
