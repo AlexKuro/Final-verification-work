15f25d178ae0b5d41e8cb0db9c0f8d5948837c38
AlexKuro <89215682813@mail.ru>
2022-12-24 23:18:11 +0300

    Метод - SearchByLines поиск нужных элементов по длине строки

--- Program.cs
+++ Program.cs
@@ -3,7 +3,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+
+
+        string[] SearchByLines(string[] text, int lenNum = 3) // поиск нужных элементов по длине строки 
+        {
+            int size = text.Length;
+            int length = 0;
+            for (int i = 0; i < size; i++)
+            {
+                if (text[i].Length <= lenNum) text[length++] = text[i];
+            }
+            string[] newText = new string[length];
+            for (int i = 0; i < length; i++) newText[i] = text[i];
+            return newText;
+        }
+
         void PrintArrayStr(string[] pArray) // печать массива тип string
         {
             Console.Write("[ ");
